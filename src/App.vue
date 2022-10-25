12d97e1211a0830522c2f0e1b515c220858f0f4f
aminkatoleukhanova <112501356+aminkatoleukhanova@users.noreply.github.com>
2022-10-26 03:16:41 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="app">
+    <link
+      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      rel="stylesheet"
+      integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+      crossorigin="anonymous"
+    >
+    <h1>VUE DATA TABLE</h1>
+    <main>
+      <h2>TABLE 1</h2>
+      <br>
+      <data-table v-bind="parametersTable1" @actionTriggered="handleAction"/>
+      <br>
+      <br>
+      <h2>TABLE 2</h2>
+      <br>
+      <data-table v-bind="parametersTable2" @actionTriggered="handleAction"/>
+    </main>
+  </div>
+</template>
+
+<style lang="css">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+main {
+  padding: 32px;
+}
+</style>
+
+<script>
+import users from "./users";
+
+export default {
+  name: "App",
+
+  computed: {
+    parametersTable1() {
+      return {
+        data: users,
+        actionMode: "multiple",
+        columnKeys: [
+          "name",
+          "email",
+          "gender",
+          "country",
+          "job",
+          "city",
+          "created_at"
+        ]
+      };
+    },
+    parametersTable2() {
+      return {
+        data: users.slice(200),
+        actionMode: "single",
+        actions: ["view"],
+        columns: [
+          { key: "id"},
+          { 
+            key: "email",
+            title: "Email address"
+          },
+          { key: "username" },
+          { key: "company" },
+          {
+            key: "phone",
+            sortable: false
+          },
+          { key: "state" }
+        ]
+      };
+    }
+  },
+
+  methods: {
+    handleAction(actionName, data) {
+      console.log(actionName, data);
+      window.alert("check out the console to see the logs");
+    }
+  }
+};
+</script>
